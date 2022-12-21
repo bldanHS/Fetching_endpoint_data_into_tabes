c607aca841deb22498c5bc8eaff8a430ab8fd004
bldanHS <dbarbatlorincz@hearsaycorp.com>
2022-12-21 15:24:03 +0100

    mmm

---
+++ frontend/main2.js
@@ -0,0 +1,135 @@
+
+const url = "http://127.0.0.1:5000"
+
+endpoints = ["/applications", "/amp", "/global"]
+
+function fetch_all_data()
+{
+  Promise.all(endpoints.map(endpoint => 
+    fetch(`${url}${endpoint}`)                
+      .then((data) => data.json())
+      .catch((logError => {
+        console.error(logError);
+      }))
+    ))
+  .then((t) => {
+    console.log(t);
+    return t;
+  })
+  .then(value => {
+    generating_table(value)
+  })
+}
+
+
+function generating_table(data) {
+  
+  //collecting the content for table headers
+  let column = [];
+  data.forEach((array) => {
+  object_keys = Object.keys(array[0]);
+  column.push(object_keys)
+  console.log(column);
+  });
+
+
+    // data = [ [{},{},{}...], [{},{},{}...], [{},{},{}...] ]
+    populating_table_head_from_2D_array_extended(column, data);
+    // data.forEach((object_array) => {
+    //    console.log(object_array)});
+                        //         let tbody = parentElement.createTBody();
+                        //             object_array.forEach((object) => {
+                        //             object_values = Object.values(object);
+                        //             let tbodyRow = tbody.insertRow();
+                        //             object_values.forEach(value => {
+                        //               let td = document.createElement("td");
+                        //               td.innerHTML = value;
+                        //               tbodyRow.appendChild(td);
+                        //               tbody.appendChild(tbodyRow);
+                        //             });
+                        //           });
+                        //       });
+                            
+                            
+                            
+}
+
+function populating_table_head_from_2D_array(list){
+  const body = document.body;
+  // debugger
+  list.forEach((list_of_key) => {
+    let parentElement = document.createElement("div");
+    tbl = document.createElement('table');
+    tbl.classList.add("table");
+    tbl.setAttribute('id',tableid)
+    // tableid++
+    let thead = tbl.createTHead();
+    let row = thead.insertRow();
+    list_of_key.forEach((key)=> {
+      let th = document.createElement("th");
+      th.innerHTML = key;
+      row.appendChild(th);
+      thead.appendChild(row);
+      tbl.appendChild(thead);
+      body.appendChild(tbl);
+      body.appendChild(parentElement);
+    })
+  });
+}
+
+
+  function populating_table_head_from_2D_array_extended(list, data){
+    const body = document.body;
+    list.forEach((list_of_key) => {
+      // let parentElement = document.createElement("div");
+      tbl = document.createElement('table');
+      tbl.classList.add("table");
+      let thead = tbl.createTHead();
+      let row = thead.insertRow();
+      let tbody = tbl.createTBody();
+      debugger
+      list_of_key.forEach((key)=> {
+        let th = document.createElement("th");
+        th.innerHTML = key;
+        row.appendChild(th);
+        thead.appendChild(row);
+        tbl.appendChild(thead);
+        body.appendChild(tbl);
+      });
+        data.forEach((array) => { // a sok foreach azert van mert nekem beljebb kell mennem a strukturaban + fuggvenybe kulon scope lesz, es nem erem el azt ami nekem kell
+          array.forEach((object) => {
+            object_values = Object.values(object);
+            let tbodyRow = tbody.insertRow();
+            object_values.forEach(value => {
+              let td = document.createElement("td");
+              td.innerHTML = value;
+              tbodyRow.appendChild(td);
+              tbody.appendChild(tbodyRow);
+            });
+          });
+        });
+    });
+  
+      
+    }
+function creating_tables(list){
+    list.forEach((list_of_key) => {
+      let parentElement = document.createElement("div");
+      table = document.createElement('table');
+      table.classList.add("table");
+      let thead = table.createTHead();
+        let row = thead.insertRow();
+        let tbody = table.createTBody();
+      list_of_key.forEach((key)=> {
+      let th = document.createElement("th");
+      th.innerHTML = key;
+      row.appendChild(th);
+      thead.appendChild(row);
+      table.appendChild(thead);
+      body.appendChild(table);
+      body.appendChild(parentElement);
+    })
+  })
+}
+
+fetch_all_data();
